210906b65e7c68c1e5ea4596c50f84d0a0ec244a
Scott Miles <sjmiles@google.com>
2013-03-15 18:44:02 -0700

    remove cruft, gate logging on logFlags

--- src/bindProperties.js
+++ src/bindProperties.js
@@ -1,36 +1,14 @@
-/* 
+/*
  * Copyright 2013 The Toolkitchen Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style
  * license that can be found in the LICENSE file.
  */
 (function() {
-  
-  /*
-  function parseAllBindingDeclarations(inRoot) {
-    forEach(inRoot.childNodes, parsBindingDeclarations, this);
-  };
-  
-  function parsBindingDeclarations(inNode) {
-    // scan for bindings in attributes
-    if (inNode.attributes) {
-      forEach(inNode.attributes, function(a) {
-        parseMustache.call(this, inNode, a.name, a.value);
-      }, this);
-    }
-  };
-  
-  function parseMustache(inNode, inName, inValue) {
-    var m = inValue.match(parseMustache.pattern);
-    if (m) {
-      //console.log(inName, inValue, m);
-      bindProperties(inNode, inName, this, m[1]);
-    }
-  };
-  parseMustache.pattern = /\{\{([^{}]*)}}/;
-  */
- 
+
+  var log = window.logFlags || {};
+
   function bindProperties(inA, inProperty, inB, inPath) {
-    console.log("[%s]: bindProperties: [%s] to [%s].[%s]", 
+    log.bind && console.log("[%s]: bindProperties: [%s] to [%s].[%s]",
         inB.localName, inPath, inA.localName, inProperty);
     var parts = inPath.split(".");
     var property = parts.pop();
@@ -48,10 +26,9 @@
       enumerable: true
     });
   }
-  
+
   // exports
-  
-  //Toolkit.parseAllBindingDeclarations = parseAllBindingDeclarations;
+
   Toolkit.bindProperties = bindProperties;
-  
+
 })();
