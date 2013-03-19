3cd0b9c4fae5930e1e4d009304794997d5ec5cde
frankiefu <frankiefu@yahoo.com>
2013-03-18 17:51:09 -0700

    added basic tests for register.js and events.js

---
+++ test/events.js
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2013 The Toolkitchen Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+suite('events', function() {
+  var assert = chai.assert;
+  
+  var work;
+  
+  setup(function() {
+    work = document.createElement('div');
+    document.body.appendChild(work);
+    // store results
+    work.innerHTML = '<div id="results" style="display: none;"></div>';
+  });
+
+  teardown(function() {
+    document.body.removeChild(work);
+  });
+  
+  function createTestElement(inName, inTemplateContent, inExtend) {
+    work.innerHTML += '<element name="' + inName + '"' + 
+        (inExtend ? ' extends="' + inExtend + '"' : '') + ' on-click="clickHandler">' +
+      '<template>' + (inTemplateContent || '') + '</template>' +
+      '<script>' +
+        'Toolkit.register(this, {' +
+          'clickHandler: function() {' +
+            'results.textContent += this.localName;' +
+          '}' +
+        '});' +
+      '</script>' +
+      '</element>';
+    new HTMLElementElement(work.lastChild);
+  }
+  
+  test('host event', function() {
+    createTestElement('x-foo');
+    var foo = document.createElement('x-foo');
+    work.appendChild(foo);
+    foo.click();
+    assert.equal(results.textContent, 'x-foo');
+  });
+  
+  /*test('host events order', function() {
+    createTestElement('x-foo');
+    createTestElement('x-bar', '<x-foo></x-foo>');
+    var bar = document.createElement('x-bar');
+    work.appendChild(bar);
+    // click on x-foo
+    bar.webkitShadowRoot.childNodes[0].click();
+    assert.equal(results.textContent, 'x-foox-bar');
+  });
+  
+  test('host events order more', function() {
+    createTestElement('x-foo');
+    createTestElement('x-bar', '<x-foo></x-foo>');
+    createTestElement('x-zot', '<x-bar></x-bar>');
+    var zot = document.createElement('x-zot');
+    work.appendChild(zot);
+    // click on x-foo
+    zot.webkitShadowRoot.childNodes[0].webkitShadowRoot.childNodes[0].click();
+    assert.equal(results.textContent, 'x-foox-barx-zot');
+  });*/
+});
---
+++ test/register.js
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2013 The Toolkitchen Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+suite('register', function() {
+  var assert = chai.assert;
+  
+  var work;
+  
+  setup(function() {
+    work = document.createElement('div');
+    document.body.appendChild(work);
+  });
+
+  teardown(function() {
+    document.body.removeChild(work);
+  });
+
+  test('register', function() {
+    work.innerHTML = '<element name="x-foo">' +
+      '<script>' +
+        'Toolkit.register(this, {' +
+          'ready: function() {' +
+            'this.message = "foo";' +
+          '},' +
+          'sayHello: function() {' +
+            'this.message = "hello";' +
+          '}' +
+        '});' +
+      '</script>' +
+      '</element>';
+    var test = new HTMLElementElement(work.querySelector('element'));
+    var foo = document.createElement('x-foo');
+    // test ready
+    assert.equal(foo.message, 'foo');
+    // test sayHello
+    foo.sayHello();
+    assert.equal(foo.message, 'hello');
+  });
+});
