170b228ea813f171c1efe7d4be4fc2ea526610e4
Scott J. Miles <sjmiles@google.com>
2013-03-25 17:53:16 -0700

    stopgap null checks in bindProperties accessors

--- src/bindProperties.js
+++ src/bindProperties.js
@@ -7,6 +7,8 @@
 
   var log = window.logFlags || {};
 
+  // bind a property in A to a path in B by converting A[property] to a
+  // getter/setter pair that accesses B[...path...]
   function bindProperties(inA, inProperty, inB, inPath) {
     log.bind && console.log("[%s]: bindProperties: [%s] to [%s].[%s]",
         inB.localName, inPath, inA.localName, inProperty);
@@ -14,19 +16,25 @@
     var property = parts.pop();
     var path = parts.length ? '.' + parts.join('.') : '';
     var getObject = new Function(['inObject'], 'return inObject' + path + ';');
-    //console.log(getObject(inB), getObject(inB)[property]);
     Object.defineProperty(inA, inProperty, {
+      // TODO(sjmiles): right now we are null-checking the penultimate object
+      // we may need to check each part of the path or wrap the entire accessor
+      // in a try/catch block
       get: function() {
-        return getObject(inB)[property];
+        var src = getObject(inB);
+        return src && src[property];
       },
       set: function(inValue) {
-        getObject(inB)[property] = inValue;
+        var tgt = getObject(inB);
+        if (tgt) {
+          tgt[property] = inValue;
+        }
       },
       configurable: true,
       enumerable: true
     });
   }
-
+ 
   // exports
 
   Toolkit.bindProperties = bindProperties;
