027055a4705ccb78bec51735187a44e8faec9ae1
Scott J. Miles <sjmiles@google.com>
2013-03-28 15:04:48 -0700

    dramatically simpler bindMDV.js (mdv[v2])

--- src/bindMDV.js
+++ src/bindMDV.js
@@ -19,94 +19,17 @@
 
   function bindModel(inRoot) {
     log.bind && console.group("[%s] bindModel", this.localName);
-    _bindModel.call(this, inRoot);
+    // TODO(sjmiles): allow 'this' to supply a 'delegate'
+    HTMLTemplateElement.bindTree(inRoot, this)
     log.bind && console.groupEnd();
   }
 
-  function _bindModel(inRoot) {
-    // establish model
-    inRoot.model = this;
-    // iterate
-    forEach(inRoot.childNodes, bindNodeModel, this);
-  }
-
-  function bindNodeModel(inNode) {
-    var n = inNode;
-    // upgraded nodes can have stale wrappers in the virtual tree
-    // TODO(sjmiles): https://github.com/toolkitchen/ShadowDOM/issues/27
-    if (n.node) {
-      n = wrap(n.node);
-    } 
-    // node's binding model is this component's model
-    n.model = this;
-    // text-node binding
-    if (n.nodeName == '#text') {
-      bindPattern.call(this, n, n.textContent);
-    }
-    // for all other non-template nodes
-    else if (n.localName !== 'template') {
-      // scan for bindings in attributes
-      bindAttributes.call(this, n);
-      // continue walking the LOCAL tree
-      _bindModel.call(this, n);
-    }
-  }
-
-  function bindAttributes(inNode) {
-    // custom binding for toolkit elements
-    var binder = inNode.isToolkitElement ? bindComponent : bindPattern;
-    // scan for bindings in attributes
-    if (inNode.attributes) {
-      forEach(inNode.attributes, function(a) {
-        binder.call(this, inNode, a.name, a.value);
-      }, this);
-    }  
-  }
-
-  function bindComponent(inNode, inName, inValue) {
-    var m = inValue.match(bindPattern.pattern);
-    if (m) {
-      Toolkit.bindProperties(inNode, inName, this, m[1]);
-    }
-  }
-
-  function bindPattern(inNode, inName, inValue) {
-    if ((inValue || inName).search(bindPattern.pattern) >= 0) {
-      addBinding.call(this, inNode, inName, inValue);
-    }
-  }
-  
-  bindPattern.pattern = /\{\{([^{}]*)}}/;
-  
-  function addBinding(inNode, inName, inValue) {
-    if (inNode.localName == 'input') {
-      addInputBinding(inNode, inName, inValue)
-    } else {
-      inNode.addBinding(inName, inValue);
-    }    
-    log.bind && console.log('[%s] addBinding: [%s].[%s] to [%s]',
-        this.localName, (inNode.localName || '#text'), inName, inValue);
-  }
-  
-  // support MDV 2-way bindings to inputs
-  function addInputBinding(inNode, inName, inValue) {
-    var m = inValue.match(bindPattern.pattern);
-    if (m) {
-      var v = m[1];
-      if (inName == 'value') {
-        inNode.addValueBinding(v);
-      } else if (inName == 'checked') {
-        inNode.addCheckedBinding(v);
-      } else {
-        inNode.addBinding(inName, inValue);
-      }
-    }
-  }
+  var mustachePattern = /\{\{([^{}]*)}}/;
 
   // exports
   
   Toolkit.bindModel = bindModel;
-  Toolkit.bindPattern = bindPattern.pattern;
+  Toolkit.bindPattern = mustachePattern;
   
 })();
 
