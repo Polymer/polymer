7e263da5d0c68f4f514aa096b3dfbcaa075e5127
frankiefu <frankiefu@yahoo.com>
2013-04-01 14:26:05 -0700

    update bindMDV mocha test

--- test/bindMDV.js
+++ test/bindMDV.js
@@ -7,7 +7,7 @@
 suite('bindMDV', function() {
   var assert = chai.assert;
   
-  test('bindModel bindPattern', function() {
+  test('bindModel bindModel', function() {
     var test = document.createElement('div');
     test.innerHTML = '<div id="a" foo="{{bar}}"></div>';
     var a = test.firstChild;
@@ -20,26 +20,8 @@ suite('bindMDV', function() {
     dirtyCheck();
     assert.equal(a.getAttribute('foo'), 8);
   });
-  
-  test('bindModel bindComponent', function() {
-    var test = document.createElement('div');
-    test.innerHTML = '<x-a id="a" foo="{{bar}}"></x-a>';
-    var a = test.firstChild;
-    // ad-hoc to make x-a a component
-    a.isToolkitElement = true;
-    //
-    Toolkit.bindModel.call(test, test);
-    test.bar = 5;
-    assert.equal(a.foo, 5);
-    //
-    test.bar = 8;
-    assert.equal(a.foo, 8);
-    // 2 ways
-    a.foo = 6;
-    assert.equal(test.bar, 6);
-  });
-  
-  test('bindModel bindInput', function() {
+    
+  test('bindModel bind input', function() {
     var test = document.createElement('div');
     test.innerHTML = '<input value="{{bar}}" />';
     var a = test.firstChild;
