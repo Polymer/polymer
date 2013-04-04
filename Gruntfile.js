42d1968481731e26d301472c045f521be5c8f6d5
Yvonne Yip <ykyyip@google.com>
2013-04-04 14:58:11 -0700

    configure browsers to test by platform

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,12 @@
 module.exports = function(grunt) {
+  var os = require('os');
+  var browsers = ['Chrome', 'Firefox'];
+  if (os.type() === 'Darwin') {
+    browsers.push('ChromeCanary');
+  }
+  if (os.type() === 'Windows_NT') {
+    browsers.push('IE');
+  }
   grunt.initConfig({
     pkg: '<json:package.json>',
     testacular: {
