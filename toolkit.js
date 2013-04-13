92c4be43ff593fff2880c164b0ccee666e3edfa1
Scott J. Miles <sjmiles@google.com>
2013-04-13 01:51:52 -0700

    rev platform

--- toolkit.js
+++ toolkit.js
@@ -4,78 +4,18 @@
  * license that can be found in the LICENSE file.
  */
 
-(function(scope) {
-
-var thisFile = 'toolkit.js';
-
-// NOTE: use attributes on the script tag for this file as directives
-// NOTE: uses 'window' and 'document' globals
-
-// acquire directives and base path from script element
-
-var attributes={}, base = '';
+/* 
+ * Copyright 2013 The Toolkitchen Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
 
 (function() {
-  var s$ = document.querySelectorAll('script[src]');
-  Array.prototype.forEach.call(s$, function(s) {
-    var src = s.getAttribute('src');
-    var re = new RegExp(thisFile + '[^\\\\]*');
-    var match = src.match(re);
-    if (match) {
-      attributes = s.attributes;
-      base = src.slice(0, -match[0].length);
-    }
-  });
-})();
-
-// flags
-
-var flags = window.flags || {};
-
-// acquire flags from script tag attributes
-
-for (var i=0, a; (a=attributes[i]); i++) {
-  flags[a.name] = a.value || true;
-}
-
-// acquire flags from url
-
-if (!flags.noOpts) {
-  location.search.slice(1).split('&').forEach(function(o) {
-    o = o.split('=');
-    flags[o[0]] = o[1] || true;
-  });
-}
-
-// process log flags
-
-var logFlags = window.logFlags || {};
-if (flags.log) {
-  flags.log.split(',').forEach(function(f) {
-    logFlags[f] = true;
-  });
-}
-window.logFlags = logFlags;
-
-// support exportas directive
-
-scope = scope || {};
-
-if (flags.exportas) {
-  window[flags.exportas] = scope;
-}
-
-// module exports
-
-scope.flags = flags;
-
-// report effective flags
-
-console.log(flags);
-
-// assemble list of dependencies
-
+  
+var thisFile = 'toolkit.js';
+var scopeName = 'Toolkit';
 var modules = [
+  'platform/platform.js',
   'src/lang.js',
   'src/oop.js',
   'src/register.js',
@@ -92,10 +32,23 @@ var modules = [
   'src/boot.js'
 ];
 
-// write script tags for dependencies
+// export 
 
-modules.forEach(function(inSrc) {
-  document.write('<script src="' + base + inSrc + '"></script>');
-});
+window[scopeName] = {
+  entryPointName: thisFile,
+  modules: modules
+};
 
+// bootstrap
+
+var script = document.querySelector('script[src*="' + thisFile + '"]');
+var src = script.attributes.src.value;
+var basePath = src.slice(0, src.indexOf(thisFile));
+
+if (!window.Loader) {
+  var path = basePath + '/tools/loader/loader.js';
+  document.write('<script src="' + path + '"></script>');
+} 
+document.write('<script>Loader.load("' + scopeName + '")</script>');
+  
 })();
