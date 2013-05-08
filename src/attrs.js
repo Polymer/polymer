0ea950819a22e65599c7ad9e19a27981a2f39ee7
Brandon Satrom <bsatrom+gh@gmail.com>
2013-05-07 20:46:19 -0500

    added deserialization of Date attributes for custom elements

--- src/attrs.js
+++ src/attrs.js
@@ -62,7 +62,7 @@
   function takeAttributes() {
     // for each attribute
     forEach(this.attributes, function(a) {
-      // try to match this attribute to a property (attributess are
+      // try to match this attribute to a property (attributes are
       // all lower-case, so this is case-insensitive search)
       var name = propertyForAttribute.call(this, a.name);
       if (name) {
@@ -87,7 +87,7 @@
 
   var lowerCase = String.prototype.toLowerCase.call.bind(
       String.prototype.toLowerCase);
-      
+     
   // return the published property matching name, or undefined
   function propertyForAttribute(name) {
     // matchable properties must be published
@@ -95,7 +95,16 @@
     // search for a matchable property
     return properties[properties.map(lowerCase).indexOf(name.toLowerCase())];
   };
-  
+
+  function createDate(year, month, day, hour, minute, second, millisecond) {
+    // hour through ms are optional and undefined if not provided. Passing 
+    // undefined into the Date constructor results in an invalid date. 
+    // Null, on the other hand, is valid.
+    return new Date(year, month-1, day,
+      hour ? hour : null, minute ? minute : null,
+      second ? second : null, millisecond ? millisecond : null);
+  };
+ 
   function deserializeValue(inValue, inDefaultValue) {
     var inferredType = typeof inDefaultValue;
     if (inferredType === 'string') {
@@ -109,6 +118,22 @@
       case 'false':
         return false;
     }
+
+    // If the default attribute value is a Date, provide a Date object
+    // for the specified value.
+    if (inDefaultValue instanceof Date) {
+      // Strip the date string into its component parts and pass each into
+      // the Date constructor.
+      var dateParts = inValue.match(/(\d+)/g);
+      
+      // length < 3 means a text format was used (ex. March 20, 2010)
+      if (dateParts.length >= 3) {
+        return createDate.apply(null, dateParts);
+      } else {
+        return new Date(inValue);
+      }
+    }
+
     var float = parseFloat(inValue);
     return (String(float) === inValue) ? float : inValue;
   }
