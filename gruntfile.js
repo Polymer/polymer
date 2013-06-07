c7f6a8dd15fe6df1dce240aae9b7944e22fd7bac
Daniel Freedman <dfreedm@google.com>
2013-06-07 12:46:16 -0700

    use full LICENSE in builds

--- gruntfile.js
+++ gruntfile.js
@@ -77,7 +77,7 @@ module.exports = function(grunt) {
     },
     uglify: {
       options: {
-        banner: '/* Copyright 2013 The Polymer Authors. All rights reserved. Use of this source code is governed by a BSD-style license that can be found in the LICENSE file. */\n'
+        banner: grunt.file.read('LICENSE')
       },
       Polymer: {
         options: {
