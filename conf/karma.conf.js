7cf2c26aa9251a77b821cae1cd9ad9bb6d4a6401
Yvonne Yip <ykyyip@google.com>
2013-08-27 11:55:03 -0700

    test: add karma-ie-launcher

--- conf/karma.conf.js
+++ conf/karma.conf.js
@@ -104,6 +104,7 @@ module.exports = function(karma) {
       'karma-mocha',
       'karma-chrome-launcher',
       'karma-firefox-launcher',
+      'karma-ie-launcher',
       'karma-script-launcher',
       'karma-crbot-reporter'
     ]
