d3948383a1efdcd38e06da74aa02d32094122079
Steve Orvell <steve@orvellia.com>
2013-09-11 16:14:17 -0700

    * ensure we prepare all sub-elements while preparing * some name factoring:   - add entered/leftView methods   - change parseElement(s) to parseDeclaration(s)   - force an element to ready itself by setting 'alwaysPrepare' to true

--- src/instance/base.js
+++ src/instance/base.js
@@ -4,92 +4,103 @@
  * license that can be found in the LICENSE file.
  */
 (function(scope) {
+  var preparingElements = 0;
 
   var base = {
     PolymerBase: true,
     job: Polymer.job,
     super: Polymer.super,
-    // user entry point for constructor-like initialization
+    // TODO(sorvell): bc, created is currently a synonym for 'ready'.
+    // We should call this in createdCallback instead of at ready time or
+    // eliminate it.
     created: function() {
     },
-    // TODO(sorvell): temporary BC
+    // user entry point for element has shadowRoot and is ready for
+    // api interaction
     ready: function() {
     },
     createdCallback: function() {
-      if (this.ownerDocument.defaultView || this.forceReady ||
-          Polymer.preparingElements) {
-        this.prepare();
+      if (this.ownerDocument.defaultView || this.alwaysPrepare ||
+          preparingElements > 0) {
+        this.prepareElement();
       }
     },
     // system entry point, do not override
-    prepare: function() {
-      if (this._prepared) {
+    prepareElement: function() {
+      if (this._elementPrepared) {
         return;
       }
-      this._prepared = true;
+      this._elementPrepared = true;
       //this.style.display = 'inline-block';
       // install property observers
       // install boilerplate attributes
       this.copyInstanceAttributes();
       // process input attributes
       this.takeAttributes();
-      // do this first so we can observe changes during initialization
-      //this.observeProperties();
       // add event listeners
       this.addHostListeners();
-      // forces sub-elements to be prepared
-      Polymer.preparingElements = true;
+      // TODO(sorvell); this is ugly but guarantees that while preparing,
+      // any sub-elements will also be prepared
+      preparingElements++;
       // process declarative resources
-      this.parseElements(this.__proto__);
-      Polymer.preparingElements = false;
-      this.observeProperties();
-      //Platform.endOfMicrotask(this.initializeProperties.bind(this));
-      // unless this element is inserted into the main document
-      // (or the user otherwise specifically prevents it)
-      // bindings will self destruct after a short time; this is 
-      // necessary to make elements collectable as garbage
-      // when polyfilling Object.observe
-      //this.asyncUnbindAll();
-      // user initialization
-      // TODO(sorvell): bc
-      //console.log('created', this);
+      this.parseDeclarations(this.__proto__);
+      preparingElements--;
+      // user entry point
       this.ready();
+      // TODO(sorvell): bc
       this.created();
-      // TODO(sorvell): refactor so this doesn't depend on properties already
-      // having been observed
+      // initialize properties and observe
+      // note that observing properties can set values that we need 
+      // to initialize to must observe before initializing...
+      this.observeProperties();
+      // TODO(sorvell): should we do this async?
       this.initializeProperties();
     },
-    enteredDocumentCallback: function() {
-      if (!this.forceReady) {
-        this.prepare();
-      }
+    enteredViewCallback: function() {
+      this.prepareElement();
       this.cancelUnbindAll(true);
       // invoke user action
+      if (this.enteredView) {
+        this.enteredView();
+      }
+      // TODO(sorvell): bc
       if (this.enteredDocument) {
         this.enteredDocument();
       }
     },
-    leftDocumentCallback: function() {
+    // TODO(sorvell): bc
+    enteredDocumentCallback: function() {
+      this.enteredViewCallback();
+    },
+    leftViewCallback: function() {
       this.asyncUnbindAll();
       // invoke user action
+      if (this.leftView) {
+        this.leftView();
+      }
+      // TODO(sorvell): bc
       if (this.leftDocument) {
         this.leftDocument();
       }
     },
+    // TODO(sorvell): bc
+    leftDocumentCallback: function() {
+      this.leftViewCallback();
+    },
     // recursive ancestral <element> initialization, oldest first
-    parseElements: function(p) {
+    parseDeclarations: function(p) {
       if (p && p.element) {
-        this.parseElements(p.__proto__);
-        p.parseElement.call(this, p.element);
+        this.parseDeclarations(p.__proto__);
+        p.parseDeclaration.call(this, p.element);
       }
     },
     // parse input <element> as needed, override for custom behavior
-    parseElement: function(elementElement) {
+    parseDeclaration: function(elementElement) {
       this.shadowFromTemplate(this.fetchTemplate(elementElement));
     },
     // return a shadow-root template (if desired), override for custom behavior
     fetchTemplate: function(elementElement) {
-      return elementElement.querySelector('template'); 
+      return elementElement.querySelector('template');
     },
     // utility function that creates a shadow root from a <template>
     shadowFromTemplate: function(template) {
