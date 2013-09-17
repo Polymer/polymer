4dc481c11505991a7c43228d3797d28f21267779
Daniel Freedman <dfreedm@google.com>
2013-09-17 15:13:41 -0700

    Realign sourcemap content files after minification

--- gruntfile.js
+++ gruntfile.js
@@ -120,7 +120,13 @@ module.exports = function(grunt) {
   grunt.registerTask('sourcemap_copy', 'Copy sourcesContent between sourcemaps', function(source, dest) {
     var sourceMap = grunt.file.readJSON(source);
     var destMap = grunt.file.readJSON(dest);
-    destMap.sourcesContent = sourceMap.sourcesContent;
+    destMap.sourcesContent = [];
+    var ssources = sourceMap.sources;
+    // uglify may reorder sources, make sure sourcesContent matches new order
+    destMap.sources.forEach(function(source) {
+      var j = ssources.indexOf(source);
+      destMap.sourcesContent.push(sourceMap.sourcesContent[j]);
+    });
     grunt.file.write(dest, JSON.stringify(destMap));
   });
   grunt.registerTask('default', ['concat_sourcemap', 'uglify', 'sourcemap_copy:polymer.concat.js.map:polymer.min.js.map', 'audit']);
