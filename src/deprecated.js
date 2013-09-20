17515da35c033702038b7cdc0b0f09b188004814
Daniel Freedman <dfreedm@google.com>
2013-09-20 16:16:30 -0700

    Remove deprecated Polymer.register

--- src/deprecated.js
+++ src/deprecated.js
@@ -5,10 +5,3 @@
  */
 
 // NOTE: put deprecated methods here that throw a useful error
-Polymer.register = function(context) {
-  if (context != window) {
-    // context is the <element> here, with a name attribute
-    var name = context.getAttribute('name');
-    throw new Error('Polymer.register is deprecated in declaration of ' + name + '. Please see http://www.polymer-project.org/getting-started.html');
-  }
-};
