24b3b3b3a1844bcca6831e91e3e190c8df8e6c74
Steve Orvell <steve@orvellia.com>
2013-09-30 11:05:32 -0700

    generalize polymer's fuoc prevention strategy; fixes: #276  - styling is now expressed via 'polymer-veiled' and 'polymer-unveil' classes which are now user customizable.  - to veil (initially hide) an element either include the class 'polymer-veiled' or add to the array: Polymer.veiledElements. This list includes 'body' by default but that can be removed to prevent body from getting de-fuoc'd.  - note: Polymer automatically unveils elements at WebComponentsReady time, but elements can be dynamically unveiled by (1) adding the polymer-veiled class to them to hide them and then (2) when they are ready, calling Polymer.unveilElements().

--- src/boot.js
+++ src/boot.js
@@ -6,15 +6,71 @@
 
 (function(scope) {
 
-// FOUC prevention tactic
-var style = document.createElement('style');
-style.textContent = 'body {opacity: 0;}';
-var head = document.querySelector('head');
-head.insertBefore(style, head.firstChild);
+  // FOUC prevention tactic
+  // default list of veiled elements
+  scope.veiledElements = ['body'];
+  // add polymer styles
+  var VEILED_CLASS = 'polymer-veiled';
+  var UNVEIL_CLASS = 'polymer-unveil';
+  var TRANSITION_TIME = 0.3;
+  var style = document.createElement('style');
+  style.textContent = '.' + VEILED_CLASS + ' { ' +
+      'visibility: hidden; opacity: 0; } \n' +
+      '.' + UNVEIL_CLASS +  '{ ' +
+      '-webkit-transition: opacity ' + TRANSITION_TIME + 's; ' +
+      'transition: opacity ' + TRANSITION_TIME +'s; }\n';
+  var head = document.querySelector('head');
+  head.insertBefore(style, head.firstChild);
 
-window.addEventListener('WebComponentsReady', function() {
-  document.body.style.webkitTransition = 'opacity 0.3s';
-  document.body.style.opacity = 1;
-});
+  // apply veiled class
+  function veilElements() {
+    var veiled = Polymer.veiledElements;
+    if (veiled) {
+      for (var i=0, l=veiled.length, u; (i<l) && (u=veiled[i]); i++) {
+        veilElementsBySelector(u);
+      }
+    }
+  }
+
+  function veilElementsBySelector(selector) {
+    var nodes = document.querySelectorAll(selector);
+    for (var i=0, l=nodes.length, n; (i<l) && (n=nodes[i]); i++) {
+      n.classList.add(VEILED_CLASS);
+    }
+  }
+
+  // apply unveil class
+  function unveilElements() {
+    requestAnimationFrame(function() {
+      var nodes = document.querySelectorAll('.' + VEILED_CLASS);
+      for (var i=0, l=nodes.length, n; (i<l) && (n=nodes[i]); i++) {
+        n.classList.add(UNVEIL_CLASS);
+        n.classList.remove(VEILED_CLASS);
+      }
+      // NOTE: depends on transition end event to remove 'unveil' class.
+      if (nodes.length) {
+        var removeUnveiled = function() {
+          for (var i=0, l=nodes.length, n; (i<l) && (n=nodes[i]); i++) {
+            n.classList.remove(UNVEIL_CLASS);
+          }
+          document.body.removeEventListener(endEvent, removeUnveiled, false);
+        }
+        document.body.addEventListener(endEvent, removeUnveiled, false);
+      };
+    });
+  }
+
+  // determine transition end event
+  var endEvent = (document.documentElement.style.webkitTransition !== undefined) ?
+      'webkitTransitionEnd' : 'transitionend';
+
+  // hookup auto-unveiling
+  document.addEventListener('DOMContentLoaded', veilElements);
+  window.addEventListener('WebComponentsReady', unveilElements);
+
+  // exports
+  // can dynamically unveil elements by adding the veiled class and then 
+  // calling Polymer.unveilElements
+  scope.unveilElements = unveilElements;
 
 })(Polymer);
