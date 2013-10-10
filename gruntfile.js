594e7b219f88e9fb787477b0118860fff7b5fcdf
Daniel Freedman <dfreedm@google.com>
2013-10-09 17:30:38 -0700

    Add WeakMap to audit log

--- gruntfile.js
+++ gruntfile.js
@@ -89,6 +89,7 @@ module.exports = function(grunt) {
           repos: [
             '.',
             '../platform',
+            '../WeakMap',
             '../ShadowDOM',
             '../HTMLImports',
             '../CustomElements',
