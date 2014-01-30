271c8a2d156508944ccef5a1f7015ae13b542cb5
Steve Orvell <steve@orvellia.com>
2014-01-29 16:50:50 -0800

    Added Polymer.import(urls, callback) to imperatively load a set of imports dynamically, asynchronously. The callback is called when the imports have loaded and any associated polymer-elements have upgraded.

---
+++ src/declaration/import.js
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2013 The Polymer Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+(function(scope) {
+
+  var whenPolymerReady = scope.whenPolymerReady;
+
+  function importElements(elementOrFragment, callback) {
+    if (elementOrFragment) {
+      var ports = elementOrFragment.querySelectorAll('link[rel=import]');
+      // normalize url...
+      for (var i=0, l=ports.length, p; (i<l) && (p=ports[i]); i++) {
+        p.href = p.href;
+      }
+      document.head.appendChild(elementOrFragment);
+      whenPolymerReady(callback);
+    } else if (callback) {
+      callback();
+    }
+  }
+
+  function importUrls(urls, callback) {
+    if (urls && urls.length) {
+        var frag = document.createDocumentFragment();
+        for (var i=0, l=urls.length, url, link; (i<l) && (url=urls[i]); i++) {
+          link = document.createElement('link');
+          link.rel = 'import';
+          link.href = url;
+          frag.appendChild(link);
+        }
+        importElements(frag, callback);
+    } else if (callback) {
+      callback();
+    }
+  }
+
+  // exports
+  scope.import = importUrls;
+  scope.importElements = importElements;
+})(Polymer);
