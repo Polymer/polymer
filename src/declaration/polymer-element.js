408ba18b4767b0f710686dd4f2912ffd048a7a31
Scott J. Miles <sjmiles@google.com>
2014-02-03 13:44:07 -0800

    begin cleaner separation of declarative from imperative registration code

--- src/declaration/polymer-element.js
+++ src/declaration/polymer-element.js
@@ -8,60 +8,42 @@
   // imports
 
   var extend = scope.extend;
-  var apis = scope.api.declaration;
+  var api = scope.api;
   var queue = scope.queue;
   var whenPolymerReady = scope.whenPolymerReady;
-
-  // imperative implementation: Polymer()
-
-  // specify an 'own' prototype for tag `name`
-  function element(name, prototype) {
-    //console.log('registering prototype [' + name + ']');
-    if (prototypesByName[name]) {
-      throw 'Already registered prototype for element ' + name;
-    }
-    // cache the prototype
-    prototypesByName[name] = prototype || {};
-    // notify the registrar waiting for 'name', if any
-    notifyPrototype(name);
-  }
+  var getRegisteredPrototype = scope.getRegisteredPrototype;
+  var waitingForPrototype = scope.waitingForPrototype;
 
   // declarative implementation: <polymer-element>
 
   var prototype = extend(Object.create(HTMLElement.prototype), {
+
     createdCallback: function() {
       if (this.getAttribute('name')) {
         this.init();
       }
     },
+
     init: function() {
-      // fetch the element name
+      // fetch declared values
       this.name = this.getAttribute('name');
-      //console.log('createdCallback', this.name);
-      // fetch our extendee name
       this.extends = this.getAttribute('extends');
+      // initiate any async resource fetches
       this.loadResources();
+      // register when all constraints are met
       this.registerWhenReady();
     },
+
     registerWhenReady: function() {
-      if (this.registered) {
-        return;
+     if (this.registered
+       || this.waitingForPrototype(this.name)
+       || this.waitingForQueue()
+       || this.waitingForResources()) {
+          return;
       }
-      // if we have no prototype, wait
-      if (this.waitingForPrototype(this.name)) {
-        return;
-      }
-      if (this.waitingForQueue()) {
-        return;
-      }
-      if (this.waitingForResources()) {
-        return;
-      }
-      this.readyToRegister();
-    },
-    readyToRegister: function() {
       queue.register(this);
     },
+
     _register: function() {
       //console.log('registering', this.name);
       //console.group('registering', this.name);
@@ -71,45 +53,53 @@
             'or one that was not registered with Polymer.', this.name,
             this.extends);
       }
-
       this.register(this.name, this.extends);
       this.registered = true;
-      registerRegistered(this.name);
       //console.groupEnd();
     },
+
     waitingForPrototype: function(name) {
       if (!getRegisteredPrototype(name)) {
         // then wait for a prototype
-        waitPrototype[name] = this;
-        // if explicitly marked as 'noscript'
-        if (this.hasAttribute('noscript') && !this.noscript) {
-          this.noscript = true;
-          // TODO(sorvell): CustomElements polyfill awareness:
-          // noscript elements should upgrade in logical order
-          // script injection ensures this under native custom elements;
-          // under imports + ce polyfills, scripts run before upgrades.
-          // dependencies should be ready at upgrade time so register
-          // prototype at this time.
-          if (window.CustomElements && !CustomElements.useNative) {
-            element(name);
-          } else {
-            var script = document.createElement('script');
-            script.textContent = 'Polymer(\'' + name + '\');';
-            this.appendChild(script);
-          }
-        }
+        waitingForPrototype(name, this);
+        // emulate script if user is not supplying one
+        this.handleNoScript(name);
+        // prototype not ready yet
         return true;
       }
     },
+
+    handleNoScript: function(name) {
+      // if explicitly marked as 'noscript'
+      if (this.hasAttribute('noscript') && !this.noscript) {
+        this.noscript = true;
+        // TODO(sorvell): CustomElements polyfill awareness:
+        // noscript elements should upgrade in logical order
+        // script injection ensures this under native custom elements;
+        // under imports + ce polyfills, scripts run before upgrades.
+        // dependencies should be ready at upgrade time so register
+        // prototype at this time.
+        if (window.CustomElements && !CustomElements.useNative) {
+          Polymer(name);
+        } else {
+          var script = document.createElement('script');
+          script.textContent = 'Polymer(\'' + name + '\');';
+          this.appendChild(script);
+        }
+      }
+    },
+
     waitingForResources: function() {
       return this._needsResources;
     },
+
     // NOTE: Elements must be queued in proper order for inheritance/composition
-    // dependency resolution. Previously this was enforced for inheritance 
+    // dependency resolution. Previously this was enforced for inheritance,
     // and by rule for composition. It's now entirely by rule.
     waitingForQueue: function() {
       return queue.wait(this);
     },
+
     loadResources: function() {
       this._needsResources = true;
       this.loadStyles(function() {
@@ -117,43 +107,19 @@
         this.registerWhenReady();
       }.bind(this));
     }
+
   });
 
   // semi-pluggable APIs 
+
   // TODO(sjmiles): should be fully pluggable (aka decoupled, currently
   // the various plugins are allowed to depend on each other directly)
-  Object.keys(apis).forEach(function(n) {
-    extend(prototype, apis[n]);
-  });
+  api.publish(api.declaration, prototype);
 
   // utility and bookkeeping
 
-  // maps tag names to prototypes
-  var prototypesByName = {};
-
-  function getRegisteredPrototype(name) {
-    return prototypesByName[name];
-  }
-
-  // elements waiting for prototype, by name
-  var waitPrototype = {};
-
-  function notifyPrototype(name) {
-    if (waitPrototype[name]) {
-      waitPrototype[name].registerWhenReady();
-      delete waitPrototype[name];
-    }
-  }
-
-  // track document.register'ed tag names
-  var registered = {};
-
-  function registerRegistered(name) {
-    registered[name] = true;
-  }
-
   function isRegistered(name) {
-    return registered[name];
+    return Boolean(HTMLElement.getPrototypeForTag(name));
   }
 
   function isCustomTag(name) {
@@ -161,26 +127,10 @@
   }
 
   // exports
+
   scope.getRegisteredPrototype = getRegisteredPrototype;
   
-  // namespace shenanigans so we can expose our scope on the registration 
-  // function
-
-  // TODO(sjmiles): find a way to do this that is less terrible
-  // copy window.Polymer properties onto `element()`
-  extend(element, scope);
-  // make window.Polymer reference `element()`
-  window.Polymer = element;
-
-  // Under the HTMLImports polyfill, scripts in the main document
-  // do not block on imports; we want to allow calls to Polymer in the main
-  // document. We do so via coordination with Platform:
-  var declarations = Platform.deliverDeclarations();
-  if (declarations) {
-    for (var i=0, l=declarations.length, d; (i<l) && (d=declarations[i]); i++) {
-      element.apply(null, d);
-    }
-  }
+  // boot tasks
 
   whenPolymerReady(function() {
     document.body.removeAttribute('unresolved');
@@ -190,5 +140,7 @@
   });
 
   // register polymer-element with document
+
   document.registerElement('polymer-element', {prototype: prototype});
+
 })(Polymer);
---
+++ src/declaration/polymer.js
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2013 The Polymer Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+(function(scope) {
+
+  // imports
+
+  var extend = scope.extend;
+  var api = scope.api;
+
+  // imperative implementation: Polymer()
+
+  // specify an 'own' prototype for tag `name`
+  function element(name, prototype) {
+    //console.log('registering prototype [' + name + ']');
+    if (getRegisteredPrototype[name]) {
+      throw 'Already registered (Polymer) prototype for element ' + name;
+    }
+    // cache the prototype
+    registerPrototype(name, prototype);
+    // notify the registrar waiting for 'name', if any
+    notifyPrototype(name);
+  }
+
+  // async prototype source
+
+  function waitingForPrototype(name, client) {
+    waitPrototype[name] = client;
+  }
+
+  var waitPrototype = {};
+
+  function notifyPrototype(name) {
+    if (waitPrototype[name]) {
+      waitPrototype[name].registerWhenReady();
+      delete waitPrototype[name];
+    }
+  }
+
+  // utility and bookkeeping
+
+  // maps tag names to prototypes, as registered with
+  // Polymer. Prototypes associated with a tag name
+  // using document.registerElement are available from
+  // HTMLElement.getPrototypeForTag().
+  // If an element was fully registered by Polymer, then
+  // Polymer.getRegisteredPrototype(name) === 
+  //   HTMLElement.getPrototypeForTag(name)
+
+  var prototypesByName = {};
+
+  function registerPrototype(name, prototype) {
+    return prototypesByName[name] = prototype || {};
+  }
+
+  function getRegisteredPrototype(name) {
+    return prototypesByName[name];
+  }
+
+  // exports
+
+  scope.getRegisteredPrototype = getRegisteredPrototype;
+  scope.waitingForPrototype = waitingForPrototype;
+
+  // namespace shenanigans so we can expose our scope on the registration 
+  // function
+
+  // make window.Polymer reference `element()`
+  //window.Polymer = element;
+  window.Polymer = function(a, b, c) {
+    if (arguments.length == 3) {
+      c.dependencies = b;
+      element(a, c)
+    } else {
+      element(a, b);
+    }
+  }
+
+  // TODO(sjmiles): find a way to do this that is less terrible
+  // copy window.Polymer properties onto `element()`
+  extend(Polymer, scope);
+
+  // Under the HTMLImports polyfill, scripts in the main document
+  // do not block on imports; we want to allow calls to Polymer in the main
+  // document. Platform collects those calls until we can process them, which
+  // we do here.
+
+  var declarations = Platform.deliverDeclarations();
+  if (declarations) {
+    for (var i=0, l=declarations.length, d; (i<l) && (d=declarations[i]); i++) {
+      element.apply(null, d);
+    }
+  }
+
+})(Polymer);
