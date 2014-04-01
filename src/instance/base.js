40bde06093289de27439809e0c5ea34b9be14b66
Steve Orvell <steve@orvellia.com>
2014-04-01 14:19:43 -0700

    Refine binding warning so it doesn't incorrectly warn when element is stamped from a template without binding.

--- src/instance/base.js
+++ src/instance/base.js
@@ -24,7 +24,7 @@
     ready: function() {
     },
     createdCallback: function() {
-      if (this.templateInstance) {
+      if (this.templateInstance && this.templateInstance.model) {
         console.warn('Attributes on ' + this.localName + ' were data bound ' +
             'prior to Polymer upgrading the element. This may result in ' +
             'incorrect binding types.');
