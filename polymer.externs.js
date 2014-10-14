579541bb3e771a8b5668b7bc161d8c8e202894c1
Peter Burns <rictic@gmail.com>
2014-10-14 10:24:55 -0700

    Add a license and an extern for the fire method
    This change is by jlklein@google.com

--- polymer.externs.js
+++ polymer.externs.js
@@ -1,4 +1,16 @@
-/** @externs @fileoverview Externs for the Polymer library. */
+/**
+ * @fileoverview Closure compiler externs for the Polymer library.
+ *
+ * @externs
+ * @license
+ * Copyright (c) 2014 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt. The complete set of authors may be
+ * found at http://polymer.github.io/AUTHORS.txt. The complete set of
+ * contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt. Code
+ * distributed by Google as part of the polymer project is also subject to an
+ * additional IP rights grant found at http://polymer.github.io/PATENTS.txt.
+ */
 
 /**
  * @param {string} name The name of the declared Polymer element.
@@ -27,6 +39,7 @@ PolymerElement.prototype.domReady = function() {};
 /** On detached from the DOM callback. */
 PolymerElement.prototype.detached = function() {};
 
+
 /**
  * Fire a callback when the light DOM children of an element changes.
  * `callback` is called at most once, and should re-register with onMutation
@@ -37,6 +50,7 @@ PolymerElement.prototype.detached = function() {};
  * @param {Function} callback The function to call when changes happen.
  */
 PolymerElement.prototype.onMutation = function(domElement, callback) {};
+
 
 /**
  * Call the callback after a given timeout.
@@ -50,6 +64,7 @@ PolymerElement.prototype.onMutation = function(domElement, callback) {};
 PolymerElement.prototype.async = function(
       callback, opt_args, opt_timeoutMillis) {};
 
+
 /**
  * Call the callback after a timeout. Calling job again with the same name
  * resets the timer but will not result in additional calls to callback.
@@ -60,3 +75,18 @@ PolymerElement.prototype.async = function(
  *     calling the callback.
  */
 PolymerElement.prototype.job = function(name, callback, timeoutMillis) {};
+
+
+/**
+ * Fire an event.
+ *
+ * @param {string} type An event name.
+ * @param {*=} detail
+ * @param {Node=} onNode Target node.
+ * @param {boolean=} bubbles Set false to prevent bubbling, defaults to true.
+ * @param {boolean=} cancelable Set false to prevent cancellation, defaults to
+ *     true.
+ * @return {Object} event
+ */
+PolymerElement.prototype.fire =
+    function(type, detail, onNode, bubbles, cancelable) {};
