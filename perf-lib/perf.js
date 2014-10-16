4ba6ca3da776107a32682bbe58a6a7c866809b2d
Steve Orvell <steve@orvellia.com>
2014-10-16 15:28:02 -0700

    reduce asynchrony in test harness in an attempt to create more consistent results.

--- perf-lib/perf.js
+++ perf-lib/perf.js
@@ -2,20 +2,15 @@ console.perf = function() {
   console.timeline();
   console.profile();
   console.perf.time = Date.now();  
-}
+};
 
 console.perfEnd = function() {
-  requestAnimationFrame(function() {
-    requestAnimationFrame(function() {
-      requestAnimationFrame(function() {
-        var time = (Date.now() - console.perf.time) + 'ms';
-        console.profileEnd();
-        document.title += ' (' + time + ')';
-        if (window.top !== window) {
-          window.top.postMessage(time, '*');
-        }
-        //console.log('From start to after first paint (?): ' + time);
-      });
-    });
-  });
-}
+  // force layout
+  document.body.offsetWidth;
+  var time = (Date.now() - console.perf.time) + 'ms';
+  console.profileEnd();
+  document.title += ' (' + time + ')';
+  if (window.top !== window) {
+    window.top.postMessage(time, '*');
+  }
+};
