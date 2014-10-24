5583be9c29c6083406743ce13b1c555c8cd2e372
Steve Orvell <steve@orvellia.com>
2014-10-23 17:58:23 -0700

    remove setTimeout; there due to CE chrome bug that we don't care about atm in this context.

--- webcomponentsjs/webcomponents-lite.js
+++ webcomponentsjs/webcomponents-lite.js
@@ -1649,7 +1649,7 @@ CustomElements.addModule(function(scope) {
       };
     }
     CustomElements.ready = true;
-    setTimeout(function() {
+    //setTimeout(function() {
       CustomElements.readyTime = Date.now();
       if (window.HTMLImports) {
         CustomElements.elapsed = CustomElements.readyTime - HTMLImports.readyTime;
@@ -1657,7 +1657,7 @@ CustomElements.addModule(function(scope) {
       document.dispatchEvent(new CustomEvent("WebComponentsReady", {
         bubbles: true
       }));
-    });
+    //});
   }
   if (typeof window.CustomEvent !== "function") {
     window.CustomEvent = function(inType, params) {
