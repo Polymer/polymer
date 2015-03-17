b5d84f002b8f576207c200e099c7c413a77902cf
Steve Orvell <steve@orvellia.com>
2015-03-17 09:53:33 -0700

    .host correctly set/unset when element is attached/detached.

--- test/unit/ready.js
+++ test/unit/ready.js
@@ -1,7 +1,7 @@
 suite('ready', function() {
 
-  var configure = ['x-ready', 'x-zot#a', 'x-zot#b', 'x-zot#c', 'x-zot#d', 'x-foo', 'x-bar', 'x-zot', 'x-bar', 'x-zot'];
-  var ready = ['x-zot#a', 'x-zot#b', 'x-zot#c', 'x-zot#d', 'x-zot', 'x-bar', 'x-zot', 'x-bar', 'x-foo', 'x-ready'];
+  var configure = ['x-ready', 'x-zot#a', 'x-zot#b', 'x-zot#c', 'x-zot#d', 'x-foo#foo', 'x-bar#bar1', 'x-zot', 'x-bar#bar2', 'x-zot'];
+  var ready = ['x-zot#a', 'x-zot#b', 'x-zot#c', 'x-zot#d', 'x-zot', 'x-bar#bar1', 'x-zot', 'x-bar#bar2', 'x-foo#foo', 'x-ready'];
 
   test('element create in dom calls configure/ready/attached in proper order', function() {
     assert.deepEqual(configureList, configure);
@@ -14,6 +14,24 @@ suite('ready', function() {
     CustomElements.takeRecords(document);
     assert.deepEqual(configureList, configure);
     assert.deepEqual(readyList, ready);
+  });
+
+  test('element.host set/unset via attach/detach', function() {
+    clearTestLists();
+    var xReady = document.body.appendChild(document.createElement('x-ready'));
+    CustomElements.takeRecords(document);
+    document.body.removeChild(xReady);
+    CustomElements.takeRecords(document);
+    assert.notOk(xReady.host);
+    assert.notOk(xReady.$.a.host);
+    assert.notOk(xReady.$.foo.host);
+    assert.notOk(xReady.$.foo.$.bar1.host);
+    document.body.appendChild(xReady);
+    CustomElements.takeRecords(document);
+    assert.notOk(xReady.host);
+    assert.equal(xReady.$.a.host, xReady);
+    assert.equal(xReady.$.foo.host, xReady);
+    assert.equal(xReady.$.foo.$.bar1.host, xReady.$.foo);
   });
 
 });
