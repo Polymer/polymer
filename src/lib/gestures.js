af30d6f9678270dba7eda7ebe66dd50b42b4a42b
Daniel Freedman <dfreedm@google.com>
2015-05-01 12:28:02 -0700

    Implement all in the burndown list
    Add a smoke test

--- src/lib/gestures.js
+++ src/lib/gestures.js
@@ -15,7 +15,9 @@
   // detect native touch action support
   var HAS_NATIVE_TA = typeof document.head.style.touchAction === 'string';
   var GESTURE_KEY = '__polymerGestures';
-  var HANDLED_MARK = '__polymerGesturesHandled';
+  var HANDLED_OBJ = '__polymerGesturesHandled';
+  var TOUCH_ACTION = '__polymerGesturesTouchAction';
+  var TAP_DISTANCE = 25;
 
   // Disabling "mouse" handlers for 500ms is enough
   var MOUSE_TIMEOUT = 500;
@@ -27,10 +29,17 @@
   // disable mouse handlers for MOUSE_TIMEOUT ms after
   // a touchend to ignore synthetic mouse events
   var MOUSE_CANCELLER = function(mouseEvent) {
-    mouseEvent[HANDLED_MARK] = true;
+    mouseEvent[HANDLED_OBJ] = {skip: true};
     // disable "ghost clicks"
     if (mouseEvent.type === 'click') {
+      var path = Polymer.dom(mouseEvent).path;
+      for (var i = 0; i < path.length; i++) {
+        if (path[i] === POINTERSTATE.mouse.target) {
+          return;
+        }
+      }
       mouseEvent.preventDefault();
+      mouseEvent.stopPropagation();
     }
   };
 
@@ -46,7 +55,47 @@
     if (set) {
       // disable MOUSE_CANCELLER after MOUSE_TIMEOUT ms
       setTimeout(IGNORE_MOUSE, MOUSE_TIMEOUT);
+    } else {
+      POINTERSTATE.mouse.target = null;
     }
+  }
+
+  var POINTERSTATE = {
+    tapPrevented: false,
+    mouse: {
+      target: null,
+    },
+    touch: {
+      x: 0,
+      y: 0,
+      id: 0,
+      scrollDecided: false
+    }
+  };
+
+  function firstTouchAction(ev) {
+    var path = Polymer.dom(ev).path;
+    var ta = 'auto';
+    for (var i = 0, n; i < path.length; i++) {
+      n = path[i];
+      if (n[TOUCH_ACTION]) {
+        ta = n[TOUCH_ACTION];
+        break;
+      }
+    }
+    return ta;
+  }
+
+  function deepTargetFind(x, y) {
+    var node = document.elementFromPoint(x, y);
+    var next = node.shadowRoot;
+    while(next) {
+      next = next.elementFromPoint(x, y);
+      if (next) {
+        node = next;
+      }
+    }
+    return node;
   }
 
   var Gestures = {
@@ -54,10 +103,7 @@
     recognizers: [],
 
     handleNative: function(ev) {
-      if (ev[HANDLED_MARK]) {
-        return;
-      }
-      ev[HANDLED_MARK] = true;
+      var handled;
       var type = ev.type;
       var node = ev.currentTarget;
       var gobj = node[GESTURE_KEY];
@@ -65,17 +111,70 @@
       if (!gs) {
         return;
       }
+      if (!ev[HANDLED_OBJ]) {
+        ev[HANDLED_OBJ] = {};
+        if (type === 'touchstart') {
+          if (POINTERSTATE.touch.id === -1) {
+            POINTERSTATE.touch.id = ev.changedTouches[0].touchIdentifier;
+          }
+        }
+        if (!HAS_NATIVE_TA) {
+          if (type === 'touchstart' || type === 'touchmove') {
+            Gestures.handleTouchAction(ev);
+          }
+        }
+        if (type === 'touchend') {
+          POINTERSTATE.mouse.target = Polymer.dom(ev).rootTarget;
+        }
+      }
+      handled = ev[HANDLED_OBJ];
+      if (handled.skip) {
+        return;
+      }
       var recognizers = Gestures.recognizers;
       // enforce gesture recognizer order
       for (var i = 0, r; i < recognizers.length; i++) {
         r = recognizers[i];
-        if (gs[r.name]) {
+        if (gs[r.name] && !handled[r.name]) {
+          handled[r.name] = true;
           r[type](ev);
         }
       }
       // ignore syntethic mouse events after a touch
       if (type === 'touchend') {
+        POINTERSTATE.touch.id = -1;
         IGNORE_MOUSE(true);
+      }
+    },
+
+    handleTouchAction: function(ev) {
+      var t = ev.changedTouches[0];
+      var type = ev.type;
+      if (type === 'touchstart') {
+        POINTERSTATE.touch.x = t.clientX;
+        POINTERSTATE.touch.y = t.clientY;
+        POINTERSTATE.touch.scrollDecided = false;
+      } else if (type === 'touchmove') {
+        if (POINTERSTATE.touch.scrollDecided) {
+          return;
+        }
+        POINTERSTATE.touch.scrollDecided = true;
+        var ta = firstTouchAction(ev);
+        var prevent = false;
+        var dx = Math.abs(POINTERSTATE.touch.x - t.clientX);
+        var dy = Math.abs(POINTERSTATE.touch.y - t.clientY);
+        if (!ev.cancelable) {
+          // scrolling is happening
+        } else if (ta === 'none') {
+          prevent = true;
+        } else if (ta === 'pan-x') {
+          prevent = dx >= dy;
+        } else if (ta === 'pan-y') {
+          prevent = dy > dx;
+        }
+        if (prevent) {
+          ev.preventDefault();
+        }
       }
     },
 
@@ -88,9 +187,6 @@
       if (!gobj) {
         node[GESTURE_KEY] = gobj = {};
       }
-      if (recognizer.touchaction) {
-        this._setupTouchAction(node, recognizer.touchaction);
-      }
       for (var i = 0, dep, gd; i < deps.length; i++) {
         dep = deps[i];
         gd = gobj[dep];
@@ -101,12 +197,15 @@
         gd[name] = (gd[name] || 0) + 1;
       }
       node.addEventListener(evType, handler);
+      if (recognizer.touchaction) {
+        this.setTouchAction(node, recognizer.touchaction);
+      }
     },
 
     register: function(recog) {
       this.recognizers.push(recog);
-      for (var i = 0; i < recog.exposes.length; i++) {
-        this.gestures[recog.exposes[i]] = recog;
+      for (var i = 0; i < recog.emits.length; i++) {
+        this.gestures[recog.emits[i]] = recog;
       }
     },
 
@@ -116,90 +215,87 @@
       if (HAS_NATIVE_TA) {
         node.style.touchAction = value;
       }
-      node.touchAction = value;
-    },
-
-    _setupTouchAction: function(node, value, info) {
-      // reuse custom value on node if set
-      var ta = node.touchAction;
-      value = ta || value;
-      // set an anchor point to see how far first move is
-      node.addEventListener('touchstart', function(e) {
-        var t = e.changedTouches[0];
-        info.initialTouch = {
-          x: t.clientX,
-          y: t.clientY
-        };
-        info.abortTrack = false;
-        info.oneshot = false;
-      });
-      node.addEventListener('touchmove', function(e) {
-        // only run this once
-        if (info.oneshot) {
-          return;
-        }
-        info.oneshot = true;
-        // "none" means always track
-        if (value === 'none') {
-          return;
-        }
-        // "auto" is default, always scroll
-        // bail-out if touch-action did its job
-        // the touchevent is non-cancelable if the page/area is scrolling
-        if (value === 'auto' || !value || (ta && !e.cancelable)) {
-          info.abortTrack = true;
-          return;
-        }
-        // check first move direction
-        // unfortunately, we can only make the decision in the first move,
-        // so we have to use whatever values are available.
-        // Typically, this can be a really small amount, :(
-        var t = e.changedTouches[0];
-        var x = t.clientX,
-          y = t.clientY;
-        var dx = Math.abs(info.initialTouch.x - x);
-        var dy = Math.abs(info.initialTouch.y - y);
-        // scroll in x axis, abort track if we move more in x direction
-        if (value === 'pan-x') {
-          info.abortTrack = dx >= dy;
-        // scroll in y axis, abort track if we move more in y direction
-        } else if (value === 'pan-y') {
-          info.abortTrack = dy >= dx;
-        }
-      });
+      node[TOUCH_ACTION] = value;
     },
 
     fire: function(target, type, detail) {
-      return target.dispatchEvent(
-        new CustomEvent(type, {
-          detail: detail,
-          bubbles: true,
-          cancelable: true
-        })
-      );
+      var ev = new CustomEvent(type, {
+        detail: detail,
+        bubbles: true,
+        cancelable: true
+      });
+      target.dispatchEvent(ev);
     }
-
   };
+
+  Gestures.register({
+    name: 'downup',
+    deps: ['mousedown', 'touchstart', 'touchend'],
+    emits: ['down', 'up'],
+
+    mousedown: function(e) {
+      var t = e.currentTarget;
+      var self = this;
+      var upfn = function upfn(e) {
+        self.fire('up', t, e);
+        document.removeEventListener('mouseup', upfn);
+      };
+      document.addEventListener('mouseup', upfn);
+      this.fire('down', t, e);
+    },
+    touchstart: function(e) {
+      this.fire('down', e.currentTarget, e.changedTouches[0]);
+    },
+    touchend: function(e) {
+      this.fire('up', e.currentTarget, e.changedTouches[0]);
+    },
+    fire: function(type, target, event) {
+      Gestures.fire(target, type, {
+        x: event.clientX,
+        y: event.clientY,
+        sourceEvent: event
+      });
+    }
+  });
 
   Gestures.register({
     name: 'track',
     touchaction: 'none',
     deps: ['mousedown', 'touchmove', 'touchend'],
+    emits: ['track'],
 
-    mousedown: function(info, e) {
+    info: {
+      state: 'start',
+      moves: [],
+      addMove: function(move) {
+        if (this.moves.length > 5) {
+          this.moves.splice(1, 1);
+        }
+        this.moves.push(move);
+      }
+    },
+
+    clearInfo: function() {
+      this.info.state = 'start';
+      this.info.moves = [];
+    },
+
+    mousedown: function(e) {
       var t = e.currentTarget;
       var self = this;
-      var movefn = function movefn(e, up) {
+      var movefn = function movefn(e) {
         // first move is 'start', subsequent moves are 'move', mouseup is 'end'
-        var state = up ? 'end' : (!info.started ? 'start' : 'move');
-        info.started = true;
-        self.fire(t, e, state);
+        self.info.state = e.type === 'mouseup' ? 'end' : 'track';
+        self.info.addMove({x: e.clientX, y: e.clientY});
+        self.fire(t, e);
         e.preventDefault();
       };
       var upfn = function upfn(e) {
-        // call mousemove function with 'end' state
-        movefn(e, true);
-        info.started = false;
+        if (self.info.state !== 'start') {
+          POINTERSTATE.tapPrevented = true;
+          movefn(e);
+        }
+        self.clearInfo();
         // remove the temporary listeners
         document.removeEventListener('mousemove', movefn);
         document.removeEventListener('mouseup', upfn);
@@ -209,82 +305,114 @@
       document.addEventListener('mouseup', upfn);
     },
 
-    touchmove: function(info, e) {
+    touchmove: function(e) {
       var t = e.currentTarget;
       var ct = e.changedTouches[0];
-      // if track was aborted, stop tracking
-      if (info.abortTrack) {
-        return;
-      }
-      e.preventDefault();
-      // the first track event is sent after some hysteresis with touchmove.
-      // Use `started` state variable to differentiate the "first" move from
-      // the rest to make track.state == 'start'
-      // first move is 'start', subsequent moves are 'move'
-      var state = !info.started ? 'start' : 'move';
-      info.started = true;
-      this.fire(t, ct, state);
+      this.info.addMove({x: ct.clientX, y: ct.clientY});
+      this.fire(t, ct);
+      this.info.state = 'track';
     },
 
-    touchend: function(info, e) {
+    touchend: function(e) {
       var t = e.currentTarget;
       var ct = e.changedTouches[0];
       // only trackend if track was started and not aborted
-      if (info.started && !info.abortTrack) {
-        // reset started state on up
-        info.started = false;
-        this.fire(t, ct, 'end');
+      if (this.info.state !== 'start') {
         // iff tracking, always prevent tap
-        e.tapPrevented = true;
+        POINTERSTATE.tapPrevented = true;
+        // reset started state on up
+        this.info.state = 'end';
+        this.info.addMove({x: ct.clientX, y: ct.clientY});
+        this.fire(t, ct);
       }
+      this.clearInfo();
     },
 
-    fire: function(target, touch, state) {
+    fire: function(target, touch) {
+      var secondlast = this.info.moves[this.info.moves.length - 2];
+      var lastmove = this.info.moves[this.info.moves.length - 1];
+      var firstmove = this.info.moves[0];
+      var dx, dy = 0;
+      if (firstmove) {
+        dx = lastmove.x - firstmove.x;
+        dy = lastmove.y - firstmove.y;
+      }
+      var ddx, ddy = 0;
+      if (secondlast) {
+        ddx = lastmove.x - secondlast.x;
+        ddy = lastmove.y - secondlast.y;
+      }
       return Gestures.fire(target, 'track', {
-        state: state,
+        state: this.info.state,
         x: touch.clientX,
-        y: touch.clientY
+        y: touch.clientY,
+        dx: dx,
+        dy: dy,
+        ddx: ddx,
+        ddy: ddy,
+        hover: function() {
+          return deepTargetFind(touch.clientX, touch.clientY);
+        }
       });
     }
 
   });
 
-  // dispatch a *bubbling* "tap" only at the node that is the target of the
-  // generating event.
-  // dispatch *synchronously* so that we can implement prevention of native
-  // actions like links being followed.
-  //
-  // TODO(dfreedm): a tap should not occur when there's too much movement.
-  // Right now, a tap can occur when a touchend happens very far from the
-  // generating touch.
-  // This *should* obviate the need for tapPrevented via track.
   Gestures.register({
     name: 'tap',
-    deps: ['click', 'touchend'],
+    deps: ['mousedown', 'click', 'touchstart', 'touchend'],
+    emits: ['tap'],
+    start: {
+      x: 0,
+      y: 0
+    },
+    reset: function() {
+      this.start.x = 0;
+      this.start.y = 0;
+    },
+    save: function(e) {
+      this.start.x = e.clientX;
+      this.start.y = e.clientY;
+    },
 
-    click: function(info, e) {
+    mousedown: function(e) {
+      this.save(e);
+    },
+    click: function(e) {
       this.forward(e);
     },
 
-    touchend: function(info, e) {
-      this.forward(e);
+    touchstart: function(e) {
+      this.save(e.changedTouches[0]);
+    },
+    touchend: function(e) {
+      this.forward(e.changedTouches[0]);
     },
 
     forward: function(e) {
-      // prevent taps from being generated from events that have been
-      // canceled or already handled via a listener lower in the tree.
-      if (!e.tapPrevented) {
-        e.tapPrevented = true;
-        this.fire(e.target);
+      var dx = Math.abs(e.clientX - this.start.x);
+      var dy = Math.abs(e.clientY - this.start.y);
+      if (dx <= TAP_DISTANCE || dy <= TAP_DISTANCE) {
+        // prevent taps from being generated if an event has canceled them
+        if (!POINTERSTATE.tapPrevented) {
+          Gestures.fire(e.target, 'tap', {
+            x: e.clientX,
+            y: e.clientY,
+            sourceEvent: e
+          });
+        }
       }
-    },
-
-    // fire a bubbling event from the generating target.
-    fire: function(target) {
-      Gestures.fire(target, 'tap', {}, true);
+      POINTERSTATE.tapPrevented = false;
+      this.reset();
     }
-
   });
+
+  var DIRECTION_MAP = {
+    'x': 'pan-x',
+    'y': 'pan-y',
+    'none': 'none',
+    'all': 'auto'
+  };
 
   // expose Polymer.Gestures as the behavior for gestures
   scope.Gestures = {
@@ -295,6 +423,10 @@
       } else {
         node.addEventListener(eventName, handler);
       }
+    },
+    setScrollDirection: function(node, direction) {
+      var ta = DIRECTION_MAP[direction] || 'auto';
+      Gestures.setTouchAction(node, ta);
     }
   };
 
