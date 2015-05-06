a0e8fe20e76a57f8360b8e008695b77ea53ce7c8
Daniel Freedman <dfreedm@google.com>
2015-05-05 17:47:41 -0700

    Fix gulp task for windows machines
    Create dist dir as needed

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp');
 var replace = require('gulp-replace');
 var shell = require('gulp-shell');
+var del = require('del');
+var fs = require('fs');
+var path = require('path');
 
 var polyclean = require('polyclean');
 
 function vulcanize(filename, dstdir, excludes) {
-  var cmd =  'node_modules/vulcanize/bin/vulcanize';
+  var cmd = path.join('node_modules', '.bin', 'vulcanize');
   if (excludes && excludes.length > 0) {
     excludes.forEach(function(exclude) {
       cmd = cmd + ' --exclude ' + exclude;
@@ -13,17 +16,17 @@ function vulcanize(filename, dstdir, excludes) {
     cmd = cmd + ' --implicit-strip';
   }
   cmd = cmd + ' --strip-comments';
-  cmd = cmd + ' ' + filename + ' > ' + dstdir + '/' + filename;
-  return cmd
+  cmd = cmd + ' ' + filename + ' > ' + path.join(dstdir, filename);
+  return cmd;
 }
 
 var micro = "polymer-micro.html";
 var mini = "polymer-mini.html";
 var max = "polymer.html";
 
-gulp.task('micro', shell.task(vulcanize(micro, 'dist')));
-gulp.task('mini', shell.task(vulcanize(mini, 'dist', [micro])));
-gulp.task('max', shell.task(vulcanize(max, 'dist', [mini, micro])));
+gulp.task('micro', ['mkdir'], shell.task(vulcanize(micro, 'dist')));
+gulp.task('mini', ['mkdir'], shell.task(vulcanize(mini, 'dist', [micro])));
+gulp.task('max', ['mkdir'], shell.task(vulcanize(max, 'dist', [mini, micro])));
 
 gulp.task('strip', ['micro', 'mini', 'max'], function() {
   return gulp.src('dist/*.html')
@@ -37,6 +40,14 @@ gulp.task('strip', ['micro', 'mini', 'max'], function() {
     .pipe(gulp.dest('dist'));
 });
 
+var workdir = 'dist';
+gulp.task('clean', function(cb) {
+  del([workdir], cb);
+});
+
+gulp.task('mkdir', ['clean'], function(cb) {
+  fs.mkdir(workdir, null, cb);
+});
 
 // Default Task
 gulp.task('default', ['strip']);
