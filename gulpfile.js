1ebc671c8b55b3d9e75b7a7edeecd9a932466ac4
Scott J Miles <sjmiles@google.com>
2015-05-05 19:08:22 -0700

    s/expr/experimental

--- gulpfile.js
+++ gulpfile.js
@@ -23,30 +23,39 @@ function vulcanize(filename, dstdir, excludes) {
 var micro = "polymer-micro.html";
 var mini = "polymer-mini.html";
 var max = "polymer.html";
+var workdir = 'dist';
 
-gulp.task('micro', ['mkdir'], shell.task(vulcanize(micro, 'dist')));
-gulp.task('mini', ['mkdir'], shell.task(vulcanize(mini, 'dist', [micro])));
-gulp.task('max', ['mkdir'], shell.task(vulcanize(max, 'dist', [mini, micro])));
+gulp.task('micro', ['mkdir'], shell.task(vulcanize(micro, workdir)));
+gulp.task('mini', ['mkdir'], shell.task(vulcanize(mini, workdir, [micro])));
+gulp.task('max', ['mkdir'], shell.task(vulcanize(max, workdir, [mini, micro])));
 
 gulp.task('strip', ['micro', 'mini', 'max'], function() {
-  return gulp.src('dist/*.html')
+  return gulp.src(['dist/'+micro, 'dist/' + mini, 'dist/' + max])
     .pipe(polyclean.cleanJsComments())
-    // Reduce script tags
-    .pipe(replace(/<\/script>\s*<script>/g, '\n'))
     // Collapse newlines
     .pipe(replace(/\n\s*\n/g, '\n'))
+    // Reduce script tags
+    .pipe(replace(/<\/script>\s*<script>/g, '\n\n'))
+    .pipe(replace('<html><head><meta charset="UTF-8">', ''))
+    .pipe(replace('</head><body>\n</body></html>', ''))
     // Collapse leading spaces+tabs.
     .pipe(replace(/^[ \t]+/gm, ''))
-    .pipe(gulp.dest('dist'));
+    // Restore important newlines
+    .pipe(replace(/(-->|<script>)/g, '$1\n'))
+    .pipe(replace(/<\/script>/g, '\n$1'))
+    // put the out
+    .pipe(gulp.dest('dist'))
+    ;
 });
 
-var workdir = 'dist';
 gulp.task('clean', function(cb) {
-  del([workdir], cb);
+  del([workdir+'/'+micro, workdir+'/'+mini, workdir+'/'+max], cb);
 });
 
 gulp.task('mkdir', ['clean'], function(cb) {
-  fs.mkdir(workdir, null, cb);
+  fs.exists(workdir, function(exists) {
+    exists ? cb() : fs.mkdir(workdir, null, cb);
+  });
 });
 
 // Default Task
