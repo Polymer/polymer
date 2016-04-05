edd44a43c2b3300668c684792c927ef4af6b39a1
Steven Orvell <sorvell@google.com>
2016-04-04 19:21:37 -0700

    update size checker.

--- size.sh
+++ size.sh
@@ -1,7 +1,3 @@
-echo 'property-effects ' && polybuild --maximum-crush src/properties/property-effects.html && gzip -c src/properties/property-effects.build.js | wc -c && rm src/properties/property-effects.build.*
-
-echo 'gesture-event-listeners ' && polybuild --maximum-crush src/events/gesture-event-listeners.html && gzip -c src/events/gesture-event-listeners.build.js | wc -c && rm src/events/gesture-event-listeners.build.*
-
 echo 'all-lib ' && polybuild --maximum-crush all-lib.html && gzip -c all-lib.build.js | wc -c && rm all-lib.build.*
 
 echo 'polymer ' && polybuild --maximum-crush polymer.html && gzip -c polymer.build.js | wc -c && rm polymer.build.*
