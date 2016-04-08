915f366ede4935bc65e96f5f1dbf123467fbcd7b
Steven Orvell <sorvell@google.com>
2016-04-08 14:30:16 -0700

    add optional delegates. template: template-stamper -> template-stamp attributes: add `ensureAttribute`

--- size.sh
+++ size.sh
@@ -1,3 +1,3 @@
-echo 'all-lib ' && polybuild --maximum-crush all-lib.html && gzip -c all-lib.build.js | wc -c && rm all-lib.build.*
+echo 'all-lib ' && polybuild all-lib.html && gzip -c all-lib.build.js | wc -c && rm all-lib.build.*
 
-echo 'polymer ' && polybuild --maximum-crush polymer.html && gzip -c polymer.build.js | wc -c && rm polymer.build.*
+echo 'polymer ' && polybuild polymer.html && gzip -c polymer.build.js | wc -c && rm polymer.build.*
