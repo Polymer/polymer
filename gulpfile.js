9e71f1aff02f16f2fb322a5f7eedad6f8efe7fda
Daniel Freedman <dfreedm@google.com>
2017-01-09 15:56:20 -0800

    it compiles!

--- gulpfile.js
+++ gulpfile.js
@@ -33,7 +33,10 @@ const ENTRY_POINTS = [POLYMER_LEGACY, POLYMER_ELEMENT];
 
 const polymer = require('polymer-build');
 const PolymerProject = polymer.PolymerProject;
-const project = new PolymerProject({ shell: DEFAULT_BUILD_TARGET });
+const project = new PolymerProject({
+  sources: ['./polymer.html'],
+  shell: './polymer.html'
+});
 const fork = polymer.forkStream;
 
 const mergeStream = require('merge-stream');
@@ -43,62 +46,91 @@ const htmlmin = require('gulp-htmlmin');
 const gzipSize = require('gzip-size');
 const prettyBytes = require('pretty-bytes');
 
-gulp.task('clean', function() {
+const closure = require('google-closure-compiler').gulp();
+
+gulp.task('clean', function () {
   return del(DIST_DIR);
 });
 
+const {Transform} = require('stream');
+
+class LogStream extends Transform {
+  constructor(prefix) {
+    super({objectMode: true});
+    this.prefix = prefix;
+  }
+  _transform(file, enc, cb) {
+    console.log(`${this.prefix}: ${file.path}`);
+    cb(null, file);
+  }
+  _flush(cb) {
+    cb();
+  }
+}
+
+class FilterStream extends Transform {
+  constructor() {
+    super({objectMode: true});
+  }
+  _transform(file, enc, cb) {
+    if (file.path.match(/polymer\.html_script_\d+\.js$/)) {
+      cb(null, file);
+    } else {
+      cb(null, null);
+    }
+  }
+}
+
 gulp.task('build', ['clean'], () => {
- // process source files in the project
- const sources = project.sources()
-   .pipe(project.splitHtml())
-   // add compilers or optimizers here!
-   .pipe(gulpif(/\.js$/, babel({presets: ['es2015']/*, plugins: ['external-helpers']*/})))
-   .pipe(gulpif(/\.js$/, uglify()))
-   .pipe(project.rejoinHtml())
-   .pipe(htmlmin({removeComments: true}));
 
- // process dependencies
- const dependencies = project.dependencies()
-   .pipe(project.splitHtml())
-   // add compilers or optimizers here!
-   .pipe(gulpif(/\.js$/, babel({presets: ['es2015']/*, plugins: ['external-helpers']*/})))
-   .pipe(gulpif(/\.js$/, uglify()))
-   .pipe(project.rejoinHtml())
-   .pipe(htmlmin({removeComments: true}));
+  const closureStream = closure({
+    // new_type_inf: true,
+    compilation_level: 'ADVANCED',
+    // compilation_level: 'SIMPLE',
+    language_in: 'ES6_STRICT',
+    language_out: 'ES5_STRICT',
+    warning_level: 'VERBOSE',
+    js_output_file: 'polymer.js',
+    output_wrapper: `${require('fs').readFileSync('LICENSE.txt')}\n(function(){\n%output%\n}).call(self)`,
+    rewrite_polyfills: false,
+    formatting: 'PRETTY_PRINT',
+    externs: ['externs/externs.js']
+  });
 
- // merge the source and dependencies streams to we can analyze the project
- const mergedFiles = mergeStream(sources, dependencies)
-   .pipe(project.analyzer);
+  // process source files in the project
+  const sources = project.sources()
 
- return mergeStream(
-   fork(mergedFiles)
-     .pipe(project.bundler)
-     // write to the bundled folder
-     .pipe(gulp.dest(BUNDLED_DIR)),
+  // process dependencies
+  const dependencies = project.dependencies()
 
-   fork(mergedFiles)
-     // write to the unbundled folder
-     .pipe(gulp.dest(UNBUNDLED_DIR))
- );
+  // merge the source and dependencies streams to we can analyze the project
+  const mergedFiles = mergeStream(sources, dependencies);
+
+  return mergedFiles
+    .pipe(project.bundler)
+    .pipe(project.splitHtml())
+    .pipe(new FilterStream())
+    .pipe(closureStream)
+    .pipe(gulp.dest(BUNDLED_DIR))
 });
 
 // copy bower.json into dist folder
-gulp.task('copy-bower-json', function() {
+gulp.task('copy-bower-json', function () {
   return gulp.src('bower.json').pipe(gulp.dest(DEFAULT_BUILD_DIR));
 });
 
 // Build
-gulp.task('build-steps', function(cb) {
+gulp.task('build-steps', function (cb) {
   runseq('restore-src', 'build', 'print-size', cb);
 });
 
 // Bundled build
-gulp.task('build-bundled', function(cb) {
+gulp.task('build-bundled', function (cb) {
   runseq('build-steps', 'save-src', 'link-bundled', cb);
 });
 
 // Unbundled build
-gulp.task('build-unbundled', function(cb) {
+gulp.task('build-unbundled', function (cb) {
   runseq('build-steps', 'save-src', 'link-unbundled', cb);
 });
 
@@ -106,58 +138,58 @@ gulp.task('build-unbundled', function(cb) {
 gulp.task('default', ['build-bundled']);
 
 // switch src and build for testing
-gulp.task('save-src', function() {
+gulp.task('save-src', function () {
   return gulp.src(ENTRY_POINTS)
-    .pipe(rename(function(p) {
+    .pipe(rename(function (p) {
       p.extname += '.src';
     }))
     .pipe(gulp.dest('.'));
 });
 
-gulp.task('restore-src', function(cb) {
-  const files = ENTRY_POINTS.map(f=>`${f}.src`);
+gulp.task('restore-src', function (cb) {
+  const files = ENTRY_POINTS.map(f => `${f}.src`);
   gulp.src(files)
-    .pipe(rename(function(p) {
+    .pipe(rename(function (p) {
       p.extname = '';
     }))
     .pipe(gulp.dest('.'))
-    .on('end', ()=>Promise.all(files.map(f=>del(f))).then(()=>cb()));
+    .on('end', () => Promise.all(files.map(f => del(f))).then(() => cb()));
 });
 
-gulp.task('link-bundled', function(cb) {
-  ENTRY_POINTS.forEach(f=>{
+gulp.task('link-bundled', function (cb) {
+  ENTRY_POINTS.forEach(f => {
     fs.writeFileSync(f, `<link rel="import" href="${DEFAULT_BUILD_DIR}/${DEFAULT_BUILD_TARGET}">`);
   });
   cb();
 });
 
-gulp.task('link-unbundled', function(cb) {
-  ENTRY_POINTS.forEach(f=>{
+gulp.task('link-unbundled', function (cb) {
+  ENTRY_POINTS.forEach(f => {
     fs.writeFileSync(f, `<link rel="import" href="${DEFAULT_BUILD_DIR}/${f}">`);
   });
   cb();
 });
 
-gulp.task('print-size', function(cb) {
-  fs.readFile(path.join(DEFAULT_BUILD_DIR, DEFAULT_BUILD_TARGET), function(err, contents) {
-    gzipSize(contents, function(err, size) {
+gulp.task('print-size', function (cb) {
+  fs.readFile(path.join(DEFAULT_BUILD_DIR, DEFAULT_BUILD_TARGET), function (err, contents) {
+    gzipSize(contents, function (err, size) {
       console.log(`${DEFAULT_BUILD_TARGET} size: ${prettyBytes(size)}`);
       cb();
     });
   });
 });
 
-gulp.task('audit', function() {
-  return gulp.src(ENTRY_POINTS.map(f=>path.join(DEFAULT_BUILD_DIR, f)))
+gulp.task('audit', function () {
+  return gulp.src(ENTRY_POINTS.map(f => path.join(DEFAULT_BUILD_DIR, f)))
     .pipe(audit('build.log', { repos: ['.'] }))
     .pipe(gulp.dest(DEFAULT_BUILD_DIR));
 });
 
-gulp.task('release', function(cb) {
+gulp.task('release', function (cb) {
   runseq('default', ['copy-bower-json', 'audit'], cb);
 });
 
-gulp.task('lint', function() {
+gulp.task('lint', function () {
   return gulp.src(['src/**/*.html', 'test/unit/*.html', 'util/*.js'])
     .pipe(eslint())
     .pipe(eslint.format())
