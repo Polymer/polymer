10f286b442049bcb2782ce5889a396af82b33726
Daniel Freedman <dfreedm@google.com>
2017-06-30 16:53:45 -0700

    [ci skip] demonstrate closure compiler error

--- externs/polymer-externs.js
+++ externs/polymer-externs.js
@@ -84,3 +84,59 @@ PolymerTelemetry.dumpRegistrations;;
 
 /** @type {PolymerTelemetry} */
 Polymer.telemetry;
+
+/** @type {boolean} */
+Polymer_PropertyAccessors.prototype.__serializing;
+/** @type {number} */
+Polymer_PropertyAccessors.prototype.__dataCounter;
+/** @type {boolean} */
+Polymer_PropertyAccessors.prototype.__dataEnabled;
+/** @type {boolean} */
+Polymer_PropertyAccessors.prototype.__dataReady;
+/** @type {boolean} */
+Polymer_PropertyAccessors.prototype.__dataInvalid;
+/** @type {!Object} */
+Polymer_PropertyAccessors.prototype.__data;
+/** @type {Object} */
+Polymer_PropertyAccessors.prototype.__dataPending;
+/** @type {Object} */
+Polymer_PropertyAccessors.prototype.__dataOld;
+/** @type {Object} */
+Polymer_PropertyAccessors.prototype.__dataProto;
+/** @type {Object} */
+Polymer_PropertyAccessors.prototype.__dataHasAccessor;
+/** @type {Object} */
+Polymer_PropertyAccessors.prototype.__dataInstanceProps;
+
+/** @type {boolean} */
+Polymer_PropertyEffects.prototype.__dataClientsReady;
+/** @type {Array} */
+Polymer_PropertyEffects.prototype.__dataPendingClients;
+/** @type {Object} */
+Polymer_PropertyEffects.prototype.__dataToNotify;
+/** @type {Object} */
+Polymer_PropertyEffects.prototype.__dataLinkedPaths;
+/** @type {boolean} */
+Polymer_PropertyEffects.prototype.__dataHasPaths;
+/** @type {Object} */
+Polymer_PropertyEffects.prototype.__dataCompoundStorage;
+/** @type {Polymer_PropertyEffects} */
+Polymer_PropertyEffects.prototype.__dataHost;
+/** @type {!Object} */
+Polymer_PropertyEffects.prototype.__dataTemp;
+/** @type {boolean} */
+Polymer_PropertyEffects.prototype.__dataClientsInitialized;
+/** @type {Object} */
+Polymer_PropertyEffects.prototype.__computeEffects;
+/** @type {Object} */
+Polymer_PropertyEffects.prototype.__reflectEffects;
+/** @type {Object} */
+Polymer_PropertyEffects.prototype.__notifyEffects;
+/** @type {Object} */
+Polymer_PropertyEffects.prototype.__propagateEffects;
+/** @type {Object} */
+Polymer_PropertyEffects.prototype.__observeEffects;
+/** @type {Object} */
+Polymer_PropertyEffects.prototype.__readOnly;
+/** @type {!TemplateInfo} */
+Polymer_PropertyEffects.prototype.__templateInfo;
