eeb71600176fa9b3b204b0631427663ca772afc7
Justin Fagnani <justin@fagnani.com>
2018-01-30 17:04:02 -0800

    Disallow non-templates as interpolations in Polymer.html (#5023)
    * Disallow non-templates as interpolations in Polymer.html
    
    * Adds `Polymer.htmlLiteral`
    
    Allows composition of strings into html templates constructed with `Polymer.html`. This is useful for composing values into styles or other places where an html template would not compose.
    
    ```
    const css = Polymer.htmlLiteral`background: red;`
    Polymer.html`<style>:host { ${css} }</style>`
    ```
    
    * lint fix and update types
    
    * Review updates.
    
    * update types.
    
    * [ci-skip] Add comment about accepting htmlLiteral values.
    
    * Update types.
    
    * Add a `toString` for output from `Polymer.htmlLiteral`
    
    Simpler for debugging

--- types/lib/utils/html-tag.d.ts
+++ types/lib/utils/html-tag.d.ts
@@ -10,19 +10,41 @@
 
 /// <reference path="boot.d.ts" />
 
+/**
+ * Class representing a static string value which can be used to filter
+ * strings by asseting that they have been created via this class. The
+ * `value` property returns the string passed to the constructor.
+ */
+declare class LiteralString {
+  value: string;
+
+  /**
+   * @returns LiteralString string value
+   */
+  toString(): string;
+}
+
 declare namespace Polymer {
 
 
   /**
-   * A template literal tag that creates an HTML <template> element from the contents of the string.
+   * A template literal tag that creates an HTML <template> element from the
+   * contents of the string.
    *
    * This allows you to write a Polymer Template in JavaScript.
    *
-   * Interpolated values are converted to strings when the template is created,
-   * they are not intended as a replacement for Polymer data-binding.
+   * Templates can be composed by interpolating `HTMLTemplateElement`s in
+   * expressions in the JavaScript template literal. The nested template's
+   * `innerHTML` is included in the containing template.  The only other
+   * values allowed in expressions are those returned from `Polymer.htmlLiteral`
+   * which ensures only literal values from JS source ever reach the HTML, to
+   * guard against XSS risks.
    *
-   * There is special support for HTMLTemplateElement values,
-   * allowing for easy composition of superclass or partial templates.
+   * All other values are disallowed in expressions to help prevent XSS
+   * attacks; however, `Polymer.htmlLiteral` can be used to compose static
+   * string values into templates. This is useful to compose strings into
+   * places that do not accept html, like the css text of a `style`
+   * element.
    *
    * Example:
    *
@@ -38,4 +60,27 @@ declare namespace Polymer {
    * @returns Constructed HTMLTemplateElement
    */
   function html(strings: TemplateStringsArray, ...values: any[]): HTMLTemplateElement;
+
+
+  /**
+   * An html literal tag that can be used with `Polymer.html` to compose.
+   * a literal string.
+   *
+   * Example:
+   *
+   *   static get template() {
+   *     return Polymer.html`
+   *       <style>
+   *         :host { display: block; }
+   *         ${styleTemplate}
+   *       </style>
+   *       <div class="shadowed">${staticValue}</div>
+   *       ${super.template}
+   *     `;
+   *   }
+   *   static get styleTemplate() { return Polymer.htmlLiteral`.shadowed { background: gray; }`; }
+   *
+   * @returns Constructed literal string
+   */
+  function htmlLiteral(strings: TemplateStringsArray, ...values: any[]): LiteralString;
 }
