ac2fa81fb7b416e27a61a4b8444c5b3c089b7362
Peter Burns <rictic@google.com>
2018-04-15 18:38:09 -0700

    Fixup a few places where comments were misplaced.

--- polymer-legacy.js
+++ polymer-legacy.js
@@ -1,15 +1,3 @@
-import { LegacyElementMixin } from './lib/legacy/legacy-element-mixin.js';
-import './lib/legacy/polymer-fn.js';
-import './lib/legacy/templatizer-behavior.js';
-import './lib/elements/dom-bind.js';
-import './lib/elements/dom-repeat.js';
-import './lib/elements/dom-if.js';
-import './lib/elements/array-selector.js';
-import './lib/elements/custom-style.js';
-import './lib/legacy/mutable-data-behavior.js';
-import { html as html$0 } from './lib/utils/html-tag.js';
-
-// bc
 /**
 @license
 Copyright (c) 2017 The Polymer Project Authors. All rights reserved.
@@ -19,11 +7,23 @@ The complete set of contributors may be found at http://polymer.github.io/CONTRI
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
 */
+
+import { LegacyElementMixin } from './lib/legacy/legacy-element-mixin.js';
+import './lib/legacy/polymer-fn.js';
 /* template elements */
+import './lib/legacy/templatizer-behavior.js';
+import './lib/elements/dom-bind.js';
+import './lib/elements/dom-repeat.js';
+import './lib/elements/dom-if.js';
+import './lib/elements/array-selector.js';
 /* custom-style */
+import './lib/elements/custom-style.js';
 /* bc behaviors */
+import './lib/legacy/mutable-data-behavior.js';
 /* import html-tag to export html */
+import { html as html$0 } from './lib/utils/html-tag.js';
+
+// bc
 export const Base = LegacyElementMixin(HTMLElement).prototype;
 
-// NOTE: this is here for modulizer to export `html` for the module version of this file
 export { html$0 as html };
--- test/unit/shady-unscoped-style-import.js
+++ test/unit/shady-unscoped-style-import.js
@@ -1,8 +1,18 @@
+/**
+@license
+Copyright (c) 2017 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+*/
+
 const $_documentContainer = document.createElement('div');
 $_documentContainer.setAttribute('style', 'display: none;');
 
 $_documentContainer.innerHTML = `<dom-module id="global-shared1">
-  
+
   <template>
     <style shady-unscoped="">
       :root {
@@ -22,23 +32,7 @@ $_documentContainer.innerHTML = `<dom-module id="global-shared1">
     </style>
   </template>
 </dom-module><dom-module id="global-shared2">
-  
+
 </dom-module>`;
 
 document.head.appendChild($_documentContainer);
-
-/**
-@license
-Copyright (c) 2017 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
-*/
-/*
-  FIXME(polymer-modulizer): the above comments were extracted
-  from HTML and may be out of place here. Review them and
-  then delete this comment!
-*/
-;
