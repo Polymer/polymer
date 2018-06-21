7fc9564e5e23aaed7e23941523c2c03cf2cdacc0
Kevin Schaaf <kschaaf@google.com>
2018-06-20 17:58:51 -0700

    Update types.

--- types/lib/legacy/class.d.ts
+++ types/lib/legacy/class.d.ts
@@ -91,7 +91,7 @@ declare namespace Polymer {
    *
    * @returns Generated class
    */
-  function Class(info: PolymerInit): {new(): HTMLElement};
+  function Class<T>(info: PolymerInit, mixin: (p0: T) => T): {new(): HTMLElement};
 }
 
 declare class PolymerGenerated {
