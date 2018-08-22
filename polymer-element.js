89cc5c62f65293e2c1a84dc606af9af7fb4df92b
Kevin Schaaf <kschaaf@google.com>
2018-08-22 15:27:30 -0700

    Remove unnecessary @const.

--- polymer-element.js
+++ polymer-element.js
@@ -25,7 +25,6 @@ export { html } from './lib/utils/html-tag.js';
  * @summary Custom element base class that provides the core API for Polymer's
  *   key meta-programming features including template stamping, data-binding,
  *   attribute deserialization, and property change observation
- * @const
  */
 export const PolymerElement = ElementMixin(HTMLElement);
 
