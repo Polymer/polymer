9eacbeb2b8378c84e78b5238fb7bc8e162268813
Peter Burns <rictic@google.com>
2019-07-19 15:47:09 -0700

    Add additional externs
    Upstreaming cl/259031976

--- externs/polymer-externs.js
+++ externs/polymer-externs.js
@@ -144,6 +144,29 @@ Polymer.syncInitialRender;
 var PolymerElement = function() {};
 
 /**
+ * The tag name of the cutom element type.
+ * @type {string|undefined}
+ */
+PolymerElement.is;
+/**
+ * The template to stamp when creating this element type.
+ * @type {!HTMLTemplateElement|undefined}
+ */
+PolymerElement.template;
+/**
+ * The properties of the cutom element type.
+ * @type {!PolymerElementProperties|undefined}
+ */
+PolymerElement.properties;
+/**
+ * The observers of this custom element type.
+ * @type {!Array<string>|undefined}
+ */
+PolymerElement.observers;
+/** @type {!PolymerInit|undefined} */
+PolymerElement.generatedFrom;
+
+/**
  * On create callback.
  * @override
  */
